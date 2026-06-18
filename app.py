7cc7b104f3d459fa60ba0864f8f2b3dbc3341f74
fasirehman232 <urfaseeh15@gmail.com>
2026-06-19 01:17:49 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
-from flask import Flask, render_template
-import os
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
-
-@app.route("/order")
-def order():
-    return "Order Page Open"
+    return "AL NOOR STORE WORKING"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(host="0.0.0.0", port=5000, debug=False)
